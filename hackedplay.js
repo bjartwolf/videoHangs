22fd183edd138ad095e3ccd1c807cb8c7e7d9102
bjartwolf <bjartnes@gmail.com>
2013-06-03 01:29:20 +0200

    fixed

--- hackedplay.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-var spawn = require('child_process').spawn;
-
-var mplayer = spawn('mplayer', ['-'])
-var videoStream = fs.createReadStream('ytdl.mp4');
-mplayer.stdout.resume();
-videoStream.pipe(mplayer.stdin);
