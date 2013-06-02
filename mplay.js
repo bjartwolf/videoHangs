6405476b1220d300b2cb05deadd32ac5e05e6c08
bjartwolf <bjartnes@gmail.com>
2013-06-03 01:28:29 +0200

    fixed

--- mplay.js
+++ mplay.js
@@ -1,7 +1,6 @@
 var fs = require('fs');
 var spawn = require('child_process').spawn;
 
-var mplayer = spawn('mplayer', ['-'])
+var mplayer = spawn('mplayer', ['-quiet', '-'])
 var videoStream = fs.createReadStream('ytdl.mp4');
-
 videoStream.pipe(mplayer.stdin);
--- play.js
+++ play.js
@@ -4,8 +4,8 @@ var spawn = require('child_process').spawn;
 var ffplay = spawn('ffplay', [
     '-autoexit',
     '-'
-]);
-
+], ['pipe', 'ignore', 'ignore']);
+ffplay.stdout.pipe(process.stdout);
 var videoStream = fs.createReadStream('ytdl.mp4');
 
 videoStream.pipe(ffplay.stdin);
